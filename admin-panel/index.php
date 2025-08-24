f93505cbb645cfcf986aba5d0d8a35f3710e736c
nelliegeorgepy <nelliegeorgepy@gmail.com>
2025-08-24 23:38:01 +0300

    Create index.php for admin panel

---
+++ admin-panel/index.php
@@ -0,0 +1,13 @@
+<?php
+// index.php
+
+// Entry point for the admin panel
+
+// Include header
+include 'includes/header.php';
+
+// Your code here
+
+// Include footer
+include 'includes/footer.php';
+?>
